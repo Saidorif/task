b4adc5e1fcdb9e6d8bc69ff13fbce3dc61618fb5
saidorif <sayyid2112@gmail.com>
2021-03-24 15:20:20 +0500

    'srv'

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -9,6 +10,7 @@ class DashboardController extends Controller
     public function index(Request $request)
     {
         $result = [];
+        $result['calendar'] = DB::select("SELECT COUNT(*) AS qty,exp_date FROM `tasks` WHERE exp_date BETWEEN '2021-01-01' AND '2021-01-31' GROUP BY exp_date");
         return response()->json(['success' => true, 'result' => $result]);
     }
 }
