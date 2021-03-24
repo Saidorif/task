e00c5c1bd58ab0c89672943b59d16716c19576e6
saidorif <sayyid2112@gmail.com>
2021-03-24 15:49:20 +0500

    'srv'

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use DB;
 use Illuminate\Http\Request;
 
@@ -9,8 +10,15 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
+        $params = $request->all();
+        $date = date('Y-m-d');
+        if(!empty($params['calendar'])){
+            $date = $params['calendar'];
+        }
+        $start_month = Carbon::parse($date)->firstOfMonth()->format('Y-m-d');
+        $end_month = Carbon::parse($date)->lastOfMonth()->format('Y-m-d');
         $result = [];
-        $result['calendar'] = DB::select("SELECT COUNT(*) AS qty,exp_date FROM `tasks` WHERE exp_date BETWEEN '2021-01-01' AND '2021-01-31' GROUP BY exp_date");
+        $result['calendar'] = DB::select("SELECT COUNT(*) AS qty,exp_date FROM `tasks` WHERE exp_date BETWEEN '$start_month' AND '$end_month' GROUP BY exp_date");
         return response()->json(['success' => true, 'result' => $result]);
     }
 }
